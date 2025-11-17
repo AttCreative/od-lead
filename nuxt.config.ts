daac8c843f76fdd8539ccaa3407cb460abc854c9
mashinoryota <mashino.ryota@att-br.jp>
2025-11-17 15:06:15 +0900

    リポジトリ直下のフォルダを削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,14 +20,19 @@ export default defineNuxtConfig({
         },
       ],
       meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
         {
           name: "description",
           content:
             "LEAD.incの最新情報やお知らせを掲載しています。サービス更新、重要なお知らせなどをご確認いただけます。",
         },
-        { name: "robots", content: "index,follow" },
+        {
+          name: "keywords",
+          content: "Read",
+        },
       ],
     },
   },
