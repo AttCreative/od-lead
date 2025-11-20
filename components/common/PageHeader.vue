031eb69db4f1904d59fe472c9d7aa78b3e30247a
mashinoryota <mashino.ryota@att-br.jp>
2025-11-20 15:19:26 +0900

    事業内容ページの実装と共通コンポーネントの実装

---
+++ components/common/PageHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page-header">
+    <div class="page-title">{{ title }}</div>
+    <div v-if="subtitle" class="page-subtitle">{{ subtitle }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  title: string;
+  subtitle?: string;
+};
+
+defineProps<Props>();
+</script>
+
+<style scoped lang="scss">
+.page-header {
+  padding-left: 80px;
+  display: flex;
+  margin: 0 auto;
+  align-items: baseline;
+  gap: 8px;
+}
+.page-title {
+  font-family: "Oswald", sans-serif;
+  color: #023482;
+  font-size: 90px;
+}
+.page-subtitle {
+  font-family: "Oswald", sans-serif;
+  color: #023482;
+  font-size: 20px;
+  font-weight: 600;
+}
+</style>
