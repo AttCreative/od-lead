f5f2934b63ff965b087521ba12db597ccfa6eb9d
mashinoryota <mashino.ryota@att-br.jp>
2025-12-28 14:56:11 +0900

    本番環境でモバイルの判定がされなかったので修正

--- composables/useIsMobile.ts
+++ composables/useIsMobile.ts
@@ -1,11 +1,8 @@
-import { isSpUserAgent } from "@/utils/isMobile";
-import { isTabletUserAgent } from "@/utils/isMobile";
+import { isSpUserAgent, isTabletUserAgent } from "@/utils/isMobile";
 
 export const useIsMobile = () => {
-  const nuxtApp = useNuxtApp();
   const isMobile = useState<boolean>("isMobile", () => false);
 
-  // SSR のとき（初回ロードや リロード）
   if (process.server) {
     const event = useRequestEvent();
     const ua =
@@ -13,34 +10,28 @@ export const useIsMobile = () => {
     isMobile.value = isSpUserAgent(ua);
   }
 
-  // クライアント遷移のとき（NuxtLink でページ移動したとき）
-  if (process.client && !nuxtApp.isHydrating) {
+  onMounted(() => {
     const ua = navigator.userAgent ?? "";
     isMobile.value = isSpUserAgent(ua);
-  }
+  });
 
   return isMobile;
 };
 
 export const useIsTablet = () => {
-  const nuxtApp = useNuxtApp();
   const isTablet = useState<boolean>("isTablet", () => false);
 
-  // SSR のとき（初回ロードや リロード）
   if (process.server) {
     const event = useRequestEvent();
     const ua =
       (event?.node.req.headers["user-agent"] as string | undefined) ?? "";
-    console.log("SSR UA:", ua);
     isTablet.value = isTabletUserAgent(ua);
   }
 
-  // クライアント遷移のとき（NuxtLink でページ移動したとき）
-  if (process.client && !nuxtApp.isHydrating) {
+  onMounted(() => {
     const ua = navigator.userAgent ?? "";
-    console.log("Client UA:", ua);
     isTablet.value = isTabletUserAgent(ua);
-  }
+  });
 
   return isTablet;
 };
